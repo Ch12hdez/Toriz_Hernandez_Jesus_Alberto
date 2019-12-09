26c5b8db5c337032205f207cd268d7ec2b028296
unitec <unitec@unitec.mx>
2019-12-09 14:50:18 -0600

    Juego
    Nivel

---
+++ Proyecto Final/Assets/Scripts/New_Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_Player : MonoBehaviour
+{
+    public float Velocidad = 5.0f;
+    public float Rotacion = 200.0f;
+    private Animator anim;
+    public float x, y;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * Rotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * Velocidad);
+
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+
+    }
+}
